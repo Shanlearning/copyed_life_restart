b1a49a2213f87b7ea9b42ae016ee0753ac526d52
Shanlearning <zhongshanyes@gmail.com>
2021-09-13 12:22:14 -0400

    Shan

--- test.py
+++ test.py
@@ -13,10 +13,20 @@ _dat = json.loads(open('data\\events.json',encoding="utf-8").read())
 
 from googletrans import Translator
 
+#from google.cloud import translate_v2 as translate
+
+
+
 translator = Translator(service_urls=['translate.googleapis.com'])
 for item in tqdm(_dat):
     _dat[item]['event'] = translator.translate(_dat[item]['event'], dest='en').text
 
+result = translator.translate(_dat[item]['event'])
 
+result.text
 
+_dat[list(_dat.keys())[120]]
 
+with open('data\\events.json', 'w') as fp:
+    json.dump(_dat, fp)  
+            
