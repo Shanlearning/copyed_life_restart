6961c28a0f39112f779e089121264215d327559d
Shanlearning <zhongshanyes@gmail.com>
2021-09-19 18:29:34 -0400

    Shan

--- src/app.js
+++ src/app.js
@@ -368,7 +368,7 @@ class App{
                 if(this.#isEnd) return;
                 const trajectory = this.#life.next();
                 const { age, content, isEnd } = trajectory;
-                const li = age $(`<li><span>${age}：</span><span>${
+                const li = $(`<li><span>${age}：</span><span>${
                     content.map(
                         ({type, description, grade, name, postEvent}) => {
                             switch(type) {
